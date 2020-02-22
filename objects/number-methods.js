d3bad3d682d6c1151769d5a68fa8c9b7994d2ea2
Jonathan White <jc.white18@outlook.com>
2020-02-22 14:21:36 -0600

    Section 5; Lesson 34 - Exploring Number Methods
    Created number-methods.js to learn more about inherited methods and Math.() methods.

---
+++ objects/number-methods.js
@@ -0,0 +1,20 @@
+let num = 103.941
+
+console.log(num.toFixed(2))
+
+console.log(Math.round(num))
+console.log(Math.floor(num))
+console.log(Math.ceil(num))
+
+let min = 0
+let max = 1
+let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
+
+
+// Challenge area
+// 1 -5 - return true if correct
+let makeGuess = function(guess) {
+    return (guess === randomNum)
+}
+
+console.log(makeGuess(1))
