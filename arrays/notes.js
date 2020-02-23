a95a6b2a7d23ef82767930a470837e48fba97e11
Jonathan White <jc.white18@outlook.com>
2020-02-22 20:48:37 -0600

    Section 6; Lesson 40 - Looping over Arrays
    Learned forEach method/loop. Implemented a small feature to number our list during output

--- arrays/notes.js
+++ arrays/notes.js
@@ -10,5 +10,9 @@ notes.splice(1, 1, 'Note 2')
 
 notes[2] = 'This is now the new note 3'
 
+notes.forEach(function(){
+    console.log('testing 123')
+})
+
 console.log(notes.length)
 console.log(notes)
--- arrays/todo.js
+++ arrays/todo.js
@@ -8,14 +8,30 @@ const todo = ['Finish JavaScript Course', 'Start Ruby on Rails Course', 'Apply f
 // Add a new item onto the end
 // Remove the first item from the list
 
-todo.splice(2, 1)
-todo.push('Howdy Yall')
-todo.shift()
+// todo.splice(2, 1)
+// todo.push('Howdy Yall')
+// todo.shift()
 
 
 const printNumOfTodo = function() {
     return `You have ${todo.length} todos`
 }
 
-console.log(printNumOfTodo())
-console.log(todo)
+    /////////////////////////
+    // HIS IMPLEMENTATION //
+    ///////////////////////
+
+/* todo.forEach(function (item, index) {
+    const num = index + 1
+    console.log(`${num}. ${item}`)
+}) */
+
+// My implementation; I would have wrapped in a function called printTodo though
+// Would make deleting something from the list with a method easier and decrimenting i
+let i = 1
+
+// my string could have been cleaner if I used import ${} instead of +
+todo.forEach(function(item) {
+    console.log(i + '. '+ item) 
+    i++
+})
