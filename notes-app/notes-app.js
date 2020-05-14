7f6aadb748acd41b4f3de37b1b226642e6fa4ef5
Jonathan White <jc.white18@outlook.com>
2020-05-14 17:48:07 -0500

    Finished Lesson 79

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -4,16 +4,20 @@ const titleElement = document.querySelector('a');
 const bodyElement = document.querySelector('#note-body');
 
 const filters = {
-    searchText: ''
+    searchText: '',
+    sortBy: 'byEdited'
 }
 
 renderNotes(notes, filters);
 
 document.querySelector('#create-note').addEventListener('click', function(event) {
+    const timestamp = moment().valueOf();
     notes.push({
         id: id,
         title: '',
-        body: ''
+        body: '',
+        createdAt: timestamp,
+        updatedAt: timestamp
     })
     saveNotes(notes);
     location.assign(`edit.html#${id}`);
@@ -25,12 +29,13 @@ document.querySelector('#search-text').addEventListener('input', function(event)
 })
 
 document.querySelector('#filter-by').addEventListener('change', function(e){
-    console.log(e.target.value);
+    filters.sortBy = e.target.value;
+    renderNotes(notes, filters);
 })
 
 window.addEventListener('storage', function(e) {
     if (e.key === 'notes') {
-        notes = JSON.parse(e.newValue)
-        renderNotes(notes, filters)
+        notes = JSON.parse(e.newValue);
+        renderNotes(notes, filters);
     }
 })
--- notes-app/notes-edit.js
+++ notes-app/notes-edit.js
@@ -1,7 +1,13 @@
+// 1. Add a DOM element between the title and body inputs (empty span)
+// 2. Set a text value: Last Edited 4 Hours Ago - moment().fromNow()
+// 3. Update value on title/body/storage change
+
 const titleElement = document.querySelector('#note-title');
+const timestampElement = document.querySelector('#timestamp');
 const bodyElement = document.querySelector('#note-body');
 const removeElement = document.querySelector('#remove-note');
 const noteId = location.hash.substring(1);
+
 let notes = getSavedNotes();
 let note = notes.find(function (note) {
     return note.id === noteId
@@ -13,14 +19,19 @@ if (note === undefined) {
 
 titleElement.value = note.title;
 bodyElement.value = note.body;
+timestampElement.textContent = generateLastEdited(note.updatedAt);
 
 titleElement.addEventListener('input', function(event){
     note.title = event.target.value;
+    note.updatedAt = moment().valueOf();
+    timestampElement.textContent = generateLastEdited(note.updatedAt);
     saveNotes(notes);
 })
 
-bodyElement.addEventListener('change', function(event){
+bodyElement.addEventListener('input', function(event){
     note.body = event.target.value;
+    note.updatedAt = moment().valueOf();
+    timestampElement.textContent = generateLastEdited(note.updatedAt);
     saveNotes(notes);
 })
 
@@ -43,5 +54,6 @@ window.addEventListener('storage', function(e) {
         
         titleElement.value = note.title;
         bodyElement.value = note.body;
+        timestampElement.textContent = generateLastEdited(note.updatedAt);
     }
 })
